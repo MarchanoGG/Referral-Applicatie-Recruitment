36d198469bc40e303ada99dc934453b5937284c8
Tobias Roessingh <tobias.roessingh@gmail.com>
2022-12-11 23:21:14 +0100

    update user add & remove

--- app/src/pages/users/UsersList.vue
+++ app/src/pages/users/UsersList.vue
@@ -26,9 +26,9 @@
           <q-td auto-width>
             <q-btn-group>
               <!-- <q-btn class="" color="secondary" dense @click="props.expand = !props.expand" :icon="'info'" /> -->
-              <q-btn class="" color="secondary" dense @click="editform = true; selected_item = props.row.object_key;"
+              <q-btn class="" color="secondary" dense @click="addform = true; selected_id = props.row.object_key;"
                 :icon="'edit'" />
-              <q-btn class="" color="secondary" dense @click="delform = true; selected_item = props.row.object_key;"
+              <q-btn class="" color="secondary" dense @click="delform = true; selected_id = props.row.object_key;"
                 :icon="'delete'" />
             </q-btn-group>
           </q-td>
@@ -54,18 +54,34 @@
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          <UserAddFormVue />
+          <q-form @submit="addItem" @reset="resetForm">
+            <div class="row">
+              <div class="col-5">
+                <q-input filled v-model="username" label="Your username *" hint="Userame" lazy-rules
+                  :rules="[val => val && val.length > 0 || 'Please type something']" />
+
+                <q-input filled label="Password *" v-model="password" :type="isPwd ? 'password' : 'text'"
+                  hint="Password" lazy-rules :rules="[val => val && val.length > 0 || 'Please type something']">
+                  <template v-slot:append>
+                    <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer"
+                      @click="isPwd = !isPwd" />
+                  </template>
+                </q-input>
+                <q-toggle v-model="recruiter" label="Is a recruiter" />
+              </div>
+            </div>
+
+            <div class="col-5">
+              <q-btn label="Submit" type="submit" color="primary" />
+              <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+            </div>
+          </q-form>
         </q-card-section>
-
-        <q-card-actions align="right" class="bg-white text-teal">
-          <q-btn flat label="OK" v-close-popup />
-        </q-card-actions>
-
       </q-card>
     </q-dialog>
 
     <!-- edit form -->
-    <q-dialog v-model="editform">
+    <!-- <q-dialog v-model="editform">
       <q-card style="width: 700px; max-width: 80vw;">
         <q-card-section>
           <div class="flex">
@@ -74,7 +90,7 @@
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          <UserEditFormVue :objectkey="selected_item" />
+          <UserEditFormVue :objectkey="selected_id" />
         </q-card-section>
 
         <q-card-actions align="right" class="bg-white text-teal">
@@ -82,7 +98,7 @@
         </q-card-actions>
 
       </q-card>
-    </q-dialog>
+    </q-dialog> -->
 
     <!-- delete form -->
     <q-dialog v-model="delform">
@@ -105,8 +121,7 @@
 
 <script>
 import { api } from 'boot/axios'
-import UserAddFormVue from './UserAddForm.vue'
-import UserEditFormVue from './UserEditForm.vue'
+import { defineComponent, ref, reactive } from 'vue'
 
 const columns = [
   {
@@ -142,46 +157,65 @@ const columns = [
     sortable: true
   }
 ]
-
-import { defineComponent, ref } from 'vue'
-
 export default defineComponent({
   name: 'UserList'
-
-  , components: {
-    UserAddFormVue,
-    UserEditFormVue,
-    // UserDeleteFormVue,
-  }
   , setup() {
-    const selected_item = ref(null)
     return {
       columns,
-      selected_item,
       pagination: { rowsPerPage: 10 },
       addform: ref(false),
-      editform: ref(false),
       delform: ref(false),
     }
   },
   data() {
     return {
-      rows: []
+      username: null,
+      password: null,
+      recruiter: false,
+      selected_id: null,
+      rows: [],
     }
   },
   methods: {
-    deleteItem() {
-      const userForm = {
-        params: { object_key: this.selected_item, }
-      }
-      api.delete('/Users', userForm)
+    // also the edit
+    addItem() {
+      const params = reactive({
+        username: this.username,
+        password: this.password,
+        recruiter: this.recruiter ? "1" : "0",
+      });
+
+      api.post('/Users', params)
         .then((response) => {
           if (response.status == 200) {
+            this.addform = false
             this.getUsers()
+            this.reset()
           }
         })
         .catch(() => {
         })
+    },
+    deleteItem() {
+      const params = {
+        params: { object_key: this.selected_id, }
+      }
+      api.delete('/Users', params)
+        .then((response) => {
+          if (response.status == 200) {
+            this.delform = false
+            this.getUsers()
+            this.reset()
+          }
+        })
+        .catch(() => {
+        })
+    },
+    resetForm() {
+      this.username = null
+      this.password = null
+      this.recruiter = false
+      this.selected_id = null
     },
     getUsers() {
       api.get('/Users')
@@ -190,7 +224,7 @@ export default defineComponent({
         })
         .catch(() => {
         })
-    }
+    },
   },
   mounted() {
     this.getUsers()
