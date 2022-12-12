582f9c7e951706f1c4715462284f308e6801ea78
Tobias Roessingh <tobias.roessingh@gmail.com>
2022-12-12 17:57:47 +0100

    Read all referrals

--- RAR/Server/RarApiConsole/dataObjects/DoCandidate.cs
+++ RAR/Server/RarApiConsole/dataObjects/DoCandidate.cs
@@ -17,6 +17,7 @@ namespace RarApiConsole.dataObjects
         [Column(TypeName = "timestamp")]
         public DateTime referred_at { get; set; }
 
+        public DoProfile? profile;
         public DoCandidate()
         {
             fk_profile = 0;
@@ -59,6 +60,13 @@ namespace RarApiConsole.dataObjects
                     if (second == true)
                     {
                         arr += ",";
+                    }
+                    foreach (var profile in myDB.profiles.ToList())
+                    {
+                        if (profile.object_key == obj.fk_profile)
+                        {
+                            obj.profile = profile;
+                        }
                     }
                     string json = JsonConvert.SerializeObject(obj);
 
--- RAR/Server/RarApiConsole/dataObjects/DoReferral.cs
+++ RAR/Server/RarApiConsole/dataObjects/DoReferral.cs
@@ -29,6 +29,11 @@ namespace RarApiConsole.dataObjects
 
         [Column(TypeName = "timestamp"), Required]
         public DateTime modification_dt { get; set; }
+        public DoUser? user;
+        public DoTask? task;
+        public DoCandidate? candidate;
+        public DoScoreboard? scoreboard;
+        public DoProfile? profile;
 
         public DoReferral()
         {
@@ -89,6 +94,42 @@ namespace RarApiConsole.dataObjects
                     {
                         arr += ",";
                     }
+                    foreach (var user in myDB.users.ToList())
+                    {
+                        if (user.object_key == obj.fk_user)
+                        {
+                            obj.user = user;
+                        }
+                    }
+                    foreach (var task in myDB.tasks.ToList())
+                    {
+                        if (task.object_key == obj.fk_task)
+                        {
+                            obj.task = task;
+                        }
+                    }
+                    foreach (var candidate in myDB.candidates.ToList())
+                    {
+                        if (candidate.object_key == obj.fk_candidate)
+                        {
+                            obj.candidate = candidate;
+                        }
+                    }
+                    foreach (var scoreboard in myDB.scoreboards.ToList())
+                    {
+                        if (scoreboard.object_key == obj.fk_scoreboard)
+                        {
+                            obj.scoreboard = scoreboard;
+                        }
+                    }
+                    foreach (var profile in myDB.profiles.ToList())
+                    {
+                        if (profile.object_key == obj.candidate?.fk_profile)
+                        {
+                            obj.profile = profile;
+                        }
+                    }
+
                     string json = JsonConvert.SerializeObject(obj);
 
                     if (json.Length > 0)
