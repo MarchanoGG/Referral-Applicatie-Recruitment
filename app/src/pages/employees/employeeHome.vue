8d8bed776e46730035c399d845a19c4dd7053589
Tobias Roessingh <tobias.roessingh@gmail.com>
2023-01-14 16:20:02 +0100

    update employee dashboard

--- app/src/pages/employees/employeeHome.vue
+++ app/src/pages/employees/employeeHome.vue
@@ -15,7 +15,53 @@
           </q-timeline-entry>
         </q-timeline>
       </div>
-      <div class="col-4 q-mx-md">
+      <div class="col-3 q-mx-md">
+      </div>
+      <div class="col-4 q-mx-md float-right">
+        <div class="">
+          <q-toolbar class="bg-primary text-white shadow-2">
+            <q-toolbar-title>Scoreboard: </q-toolbar-title>
+          </q-toolbar>
+
+          <q-list bordered>
+            <q-item v-for="contact in contacts" :key="contact.id" class="q-my-sm" clickable v-ripple>
+              <q-item-section avatar>
+                <q-avatar color="primary" text-color="white">
+                  {{ contact.letter }}
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>{{ contact.name }}</q-item-label>
+                <q-item-label caption lines="1">{{ contact.email }}</q-item-label>
+              </q-item-section>
+
+              <q-item-section side>
+                <q-icon name="chat_bubble" color="green" />
+              </q-item-section>
+            </q-item>
+
+            <q-separator />
+            <q-item-label header>Offline</q-item-label>
+
+            <q-item v-for="contact in offline" :key="contact.id" class="q-mb-sm" clickable v-ripple>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img :src="`https://cdn.quasar.dev/img/${contact.avatar}`">
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>{{ contact.name }}</q-item-label>
+                <q-item-label caption lines="1">{{ contact.email }}</q-item-label>
+              </q-item-section>
+
+              <q-item-section side>
+                <q-icon name="chat_bubble" color="grey" />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
       </div>
     </div>
 
