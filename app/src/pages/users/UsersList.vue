1ed9a4ee794f14abe165419acdb1a8e53bc8f986
Tobias Roessingh <tobias.roessingh@gmail.com>
2023-01-19 22:41:16 +0100

    fix user crud

--- app/src/pages/users/UsersList.vue
+++ app/src/pages/users/UsersList.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="q-pa-md">
-    <q-table dense :rows="tablerows" :columns="columns" row-key="id" :pagination="pagination">
+    <q-table dense :rows="userStore.tablerows" :columns="columns" row-key="id" :pagination="pagination">
       <template v-slot:top>
         <q-toolbar>
           <q-toolbar-title :shrink="true">Users</q-toolbar-title>
@@ -25,9 +25,9 @@
           <q-td auto-width>
             <q-btn-group>
               <!-- <q-btn class="" color="secondary" dense @click="props.expand = !props.expand" :icon="'info'" /> -->
-              <q-btn class="" color="secondary" dense @click="editform = true; selected_item = props.row;"
+              <q-btn class="" color="secondary" dense @click="editform = true; userStore.selected_item = props.row;"
                 :icon="'edit'" />
-              <q-btn class="" color="secondary" dense @click="delform = true; selected_item = props.row;"
+              <q-btn class="" color="secondary" dense @click="delform = true; userStore.selected_item = props.row;"
                 :icon="'delete'" />
             </q-btn-group>
           </q-td>
@@ -44,7 +44,7 @@
 
     </q-table>
 
-    <q-dialog v-model="addform" @hide="resetForm">
+    <q-dialog v-model="addform" @hide="userStore.resetItem()">
       <q-card style="width: 700px; max-width: 80vw;">
         <q-card-section>
           <div class="flex">
@@ -53,38 +53,40 @@
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          <q-form @submit="addItem" @reset="resetForm">
+          <q-form @submit="userStore.addItem(); addform = userStore.has_errors" @reset="userStore.resetItem()">
             <div class="row">
               <div class="col-5">
-                <q-input filled v-model="selected_item.username" label="Your username *" hint="Userame" lazy-rules
-                  :rules="[val => val && val.length > 0 || 'Please type something']" />
+                <q-input filled v-model="userStore.selected_item.username" label="Your username *" hint="Userame"
+                  lazy-rules :rules="[val => val && val.length > 0 || 'Please type something']" />
 
-                <q-input filled label="Password *" v-model="selected_item.password" :type="isPwd ? 'password' : 'text'"
-                  hint="Password" lazy-rules :rules="[val => val && val.length > 0 || 'Please type something']">
+                <q-input filled label="Password *" v-model="userStore.selected_item.password"
+                  :type="isPwd ? 'password' : 'text'" hint="Password" lazy-rules
+                  :rules="[val => val && val.length > 0 || 'Please type something']">
                   <template v-slot:append>
                     <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer"
                       @click="isPwd = !isPwd" />
                   </template>
                 </q-input>
-                <q-toggle v-model="selected_item.recruiter_bool" label="Is a recruiter?" />
+                <q-toggle v-model="userStore.selected_item.recruiter_bool" label="Is a recruiter?" />
               </div>
 
               <div class="col-5">
-                <q-input filled v-model="selected_item.profile.initials" label="Your initials" hint="Initials"
+                <q-input filled v-model="userStore.selected_item.profile.initials" label="Your initials" hint="Initials"
                   lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.name" label="Your firstname" hint="Firstname"
+                <q-input filled v-model="userStore.selected_item.profile.name" label="Your firstname" hint="Firstname"
                   lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.surname" label="Your surname" hint="Surname"
+                <q-input filled v-model="userStore.selected_item.profile.surname" label="Your surname" hint="Surname"
                   lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.email" label="Your email" hint="Email" lazy-rules />
+                <q-input filled v-model="userStore.selected_item.profile.email" label="Your email" hint="Email"
+                  lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.phone_number" label="Your phone number"
+                <q-input filled v-model="userStore.selected_item.profile.phone_number" label="Your phone number"
                   hint="Phone number" lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.address" label="Your address" hint="Address"
+                <q-input filled v-model="userStore.selected_item.profile.address" label="Your address" hint="Address"
                   lazy-rules />
               </div>
             </div>
@@ -99,7 +101,7 @@
     </q-dialog>
 
     <!-- edit form -->
-    <q-dialog v-model="editform" @hide="resetForm">
+    <q-dialog v-model="editform" @hide="userStore.resetItem()">
       <q-card style="width: 700px; max-width: 80vw;">
         <q-card-section>
           <div class="flex">
@@ -108,38 +110,40 @@
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          <q-form @submit="editItem" @reset="resetForm">
+          <q-form @submit="userStore.editItem(); editform = userStore.has_errors" @reset="userStore.resetItem()">
             <div class="row">
               <div class="col-5">
-                <q-input filled v-model="selected_item.username" label="Your username *" hint="Userame" lazy-rules
-                  :rules="[val => val && val.length > 0 || 'Please type something']" />
+                <q-input filled v-model="userStore.selected_item.username" label="Your username *" hint="Userame"
+                  lazy-rules :rules="[val => val && val.length > 0 || 'Please type something']" />
 
-                <q-input filled label="Password *" v-model="selected_item.password" :type="isPwd ? 'password' : 'text'"
-                  hint="Password" lazy-rules :rules="[val => val && val.length > 0 || 'Please type something']">
+                <q-input filled label="Password *" v-model="userStore.selected_item.password"
+                  :type="isPwd ? 'password' : 'text'" hint="Password" lazy-rules
+                  :rules="[val => val && val.length > 0 || 'Please type something']">
                   <template v-slot:append>
                     <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer"
                       @click="isPwd = !isPwd" />
                   </template>
                 </q-input>
-                <q-toggle v-model="selected_item.recruiter_bool" label="Is a recruiter" />
+                <q-toggle v-model="userStore.selected_item.recruiter_bool" label="Is a recruiter" />
               </div>
 
               <div class="col-5">
-                <q-input filled v-model="selected_item.profile.initials" label="Your initials" hint="Initials"
+                <q-input filled v-model="userStore.selected_item.profile.initials" label="Your initials" hint="Initials"
                   lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.name" label="Your firstname" hint="Firstname"
+                <q-input filled v-model="userStore.selected_item.profile.name" label="Your firstname" hint="Firstname"
                   lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.surname" label="Your surname" hint="Surname"
+                <q-input filled v-model="userStore.selected_item.profile.surname" label="Your surname" hint="Surname"
                   lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.email" label="Your email" hint="Email" lazy-rules />
+                <q-input filled v-model="userStore.selected_item.profile.email" label="Your email" hint="Email"
+                  lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.phone_number" label="Your phone number"
+                <q-input filled v-model="userStore.selected_item.profile.phone_number" label="Your phone number"
                   hint="Phone number" lazy-rules />
 
-                <q-input filled v-model="selected_item.profile.address" label="Your address" hint="Address"
+                <q-input filled v-model="userStore.selected_item.profile.address" label="Your address" hint="Address"
                   lazy-rules />
               </div>
             </div>
@@ -155,7 +159,7 @@
     </q-dialog>
 
     <!-- delete form -->
-    <q-dialog v-model="delform" @hide="resetForm">
+    <q-dialog v-model="delform" @hide="userStore.resetItem()">
       <q-card style="width: 700px; max-width: 80vw;">
         <q-card-section>
           <div class="flex">
@@ -164,7 +168,8 @@
         </q-card-section>
 
         <q-card-actions class="bg-white ">
-          <q-btn @click="deleteItem" label="Delete" type="submit" color="primary" />
+          <q-btn @click="userStore.deleteItem(); delform = userStore.has_errors" label="Delete" type="submit"
+            color="primary" />
           <q-btn label="Cancel" v-close-popup color="primary" flat class="float-right" />
         </q-card-actions>
 
@@ -174,8 +179,8 @@
 </template>
 
 <script>
-import { api } from 'boot/axios'
 import { defineComponent, ref, reactive, computed } from 'vue'
+import { useUserStore } from "stores/user";
 
 const columns = [
   {
@@ -214,115 +219,21 @@ const columns = [
 export default defineComponent({
   name: 'UserList',
   setup() {
+    const userStore = useUserStore();
+    userStore.all()
     return {
       columns,
       pagination: { rowsPerPage: 10 },
       addform: ref(false),
       delform: ref(false),
       editform: ref(false),
+      userStore,
     }
   },
   data() {
-    const default_item = {
-      username: null,
-      password: null,
-      recruiter: 0,
-      recruiter_bool: false,
-      recruiter_str: "",
-      fk_profile: null,
-      object_key: null,
-      profile: {
-        initials: null,
-        name: null,
-        surname: null,
-        email: null,
-        phone_number: null,
-        address: null,
-      }
-    }
     return {
-      rows: [],
       isPwd: false,
-      default_item: default_item,
-      selected_item: default_item,
     }
-  },
-  computed: {
-    tablerows() {
-      return this.rows.map(this.readyRowItem)
-    }
-  },
-  methods: {
-    readyRowItem(item) {
-      item.recruiter_bool = computed({
-        get: () => Boolean(item.recruiter),
-        set: (val) => {
-          item.recruiter = Number(val)
-        }
-      })
-      item.recruiter_str = computed({
-        get: () => item.recruiter ? 'Yes' : 'No',
-        set: (val) => {
-          item.recruiter = Number(val)
-        }
-      })
-      return item
-    },
-    addItem() {
-      api.post('/Users', this.selected_item)
-        .then((response) => {
-          if (response.status == 200) {
-            this.addform = false
-            this.getUsers()
-            this.resetForm()
-          }
-        })
-        .catch(() => {
-        })
-    },
-    editItem() {
-      api.put('/Users', this.selected_item)
-        .then((response) => {
-          if (response.status == 200) {
-            this.editform = false
-            this.getUsers()
-            this.resetForm()
-          }
-        })
-        .catch(() => {
-        })
-    },
-    deleteItem() {
-      const params = {
-        params: { object_key: this.selected_item.object_key, }
-      }
-      api.delete('/Users', params)
-        .then((response) => {
-          if (response.status == 200) {
-            this.delform = false
-            this.getUsers()
-            this.resetForm()
-          }
-        })
-        .catch(() => {
-        })
-    },
-    resetForm() {
-      this.selected_item = this.readyRowItem(this.default_item)
-    },
-    getUsers() {
-      api.get('/Users')
-        .then((response) => {
-          if (response.data && response.data.length > 0) {
-            this.rows = response.data
-          }
-        })
-        .catch(() => {
-        })
-    },
-  },
-  mounted() {
-    this.getUsers()
   },
 })
 </script>
--- app/src/stores/user.js
+++ app/src/stores/user.js
@@ -1,14 +1,53 @@
 import { defineStore } from "pinia";
 import { useRouter } from "vue-router";
 import { api } from "boot/axios";
+import { defineComponent, ref, reactive, computed } from "vue";
 
 export const useUserStore = defineStore("user", {
   state: () => ({
     user: JSON.parse(localStorage.getItem("user")),
     router: useRouter(),
+    default_item: {
+      username: null,
+      password: null,
+      recruiter: 0,
+      recruiter_bool: false,
+      recruiter_str: "",
+      fk_profile: null,
+      object_key: null,
+      profile: {
+        initials: null,
+        name: null,
+        surname: null,
+        email: null,
+        phone_number: null,
+        address: null,
+      },
+    },
+    selected_item: {
+      username: null,
+      password: null,
+      recruiter: 0,
+      recruiter_bool: false,
+      recruiter_str: "",
+      fk_profile: null,
+      object_key: null,
+      profile: {
+        initials: null,
+        name: null,
+        surname: null,
+        email: null,
+        phone_number: null,
+        address: null,
+      },
+    },
+    items: [],
+    has_errors: false,
+    last_res: null,
   }),
   getters: {
     currentUser: (state) => state.user,
+    tablerows: (state) => state.items.map(state.readyRowItem),
   },
   actions: {
     async fetchUser() {
@@ -38,6 +77,82 @@ export const useUserStore = defineStore("user", {
       this.user = null;
       localStorage.removeItem("user");
       this.router.push("/login");
+    },
+    readyRowItem(item) {
+      item.recruiter_bool = computed({
+        get: () => Boolean(item.recruiter),
+        set: (val) => {
+          item.recruiter = Number(val);
+        },
+      });
+      item.recruiter_str = computed({
+        get: () => (item.recruiter ? "Yes" : "No"),
+        set: (val) => {
+          item.recruiter = Number(val);
+        },
+      });
+      return item;
+    },
+    focusItem(item) {
+      this.selected_item = { ...this.selected_item, ...item };
+    },
+    formItem() {
+      return this.selected_item;
+    },
+    async all() {
+      this.last_res = await api.get("/Users");
+      if (this.last_res.status == 200) {
+        this.items = this.last_res?.data;
+      } else {
+        this.has_errors = true;
+      }
+    },
+    async allByUser() {
+      this.last_res = await api.get("/Users", {
+        params: { fk_user: this.userStore.user.object_key },
+      });
+      if (this.last_res.status == 200) {
+        this.items = this.last_res?.data;
+      } else {
+        this.has_errors = true;
+      }
+    },
+    async addItem() {
+      this.last_res = await api.post("/Users", this.formItem());
+      if (this.last_res.status == 200) {
+        this.selected_item = this.last_res?.data[0];
+        this.all();
+        this.resetItem();
+      } else {
+        this.has_errors = true;
+      }
+    },
+    async editItem() {
+      this.last_res = await api.put("/Users", this.formItem());
+      if (this.last_res.status == 200) {
+        this.all();
+        this.resetItem();
+      } else {
+        this.has_errors = true;
+      }
+    },
+    async deleteItem() {
+      this.last_res = await api.delete("/Users", {
+        params: { object_key: this.selected_item.object_key },
+      });
+      if (this.last_res.status == 200) {
+        this.all();
+        this.resetItem();
+      } else {
+        this.has_errors = true;
+      }
+    },
+    resetItem() {
+      this.selected_item = this.readyRowItem(this.default_item);
+      this.has_errors = false;
+    },
+    dateRange() {
+      return this.selected_item.start_dt;
     },
   },
 });
